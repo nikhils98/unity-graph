171cf29acdfdcf4140587dd74099e7796a1eed0a
Nikhil Satiani <nikhil.satiani@gmail.com>
2024-05-19 13:13:27 +0500

    add z dimension

--- Assets/FunctionLibrary.cs
+++ Assets/FunctionLibrary.cs
@@ -5,30 +5,39 @@ using static UnityEngine.Mathf;
 
 public class FunctionLibrary
 {
-    static readonly Function[] functions = { Wave, MultiWave };
+    static readonly Function[] functions = { Wave, MultiWave, Ripple };
 
     public enum FunctionName
     {
         Wave,
-        MultiWave
+        MultiWave,
+        Ripple
     }
 
-    public delegate float Function(float x, float t);
+    public delegate float Function(float x, float z, float t);
 
     public static Function GetFunction(FunctionName name)
     {
         return functions[(int)name];
     }
 
-    static float Wave(float x, float t)
+    static float Wave(float x, float z, float t)
     {
-        return Sin(PI * (x + t));
+        return Sin(PI * (x + z + t));
     }
 
-    static float MultiWave(float x, float t)
+    static float MultiWave(float x, float z, float t)
     {
         float y = Sin(PI * (x + t));
-        y += 0.5f * Sin(2f * PI * (x + t));
+        y += 0.5f * Sin(2f * PI * (z + t));
+        y += Sin(PI * (x + z + 0.25f * t));
         return y * (2f / 3f);
+    }
+
+    static float Ripple(float x, float z, float t)
+    {
+        float d = Sqrt(x * x + z * z);
+        float y = Sin(PI * (4f * d - t));
+        return y / (1f + 10f * d);
     }
 }
--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -21,13 +21,20 @@ public class Graph : MonoBehaviour
         var position = Vector3.zero;
         var scale = Vector3.one * step;
 
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
 
-        for (var i = 0; i < resolution; i++)
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
+
             var point = points[i] = Instantiate(pointPrefab);
 
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
 
             point.localPosition = position;
             point.localScale = scale;
@@ -40,11 +47,11 @@ public class Graph : MonoBehaviour
         var time = Time.time;
         var f = FunctionLibrary.GetFunction(function);
 
-        for (var i = 0; i < resolution; i++)
+        for (var i = 0; i < points.Length; i++)
         {
             var point = points[i];
             var position = point.localPosition;
-            position.y = f(position.x, time);
+            position.y = f(position.x, position.z, time);
             point.localPosition = position;
         }
     }
